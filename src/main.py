57acc54f0a609cc02d0e19f3c8a20bba340d6e75
Kishan200416 <ghetiakishan16@gmail.com>
2025-10-30 23:16:01 -0600

    Implemented the logic for countdown and applied to the Button

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ BUTTON_COLOR = "#DAA520"
 FONT_NAME = "Press Start 2P" # Got it from google
 
 # ---Timer Constants---
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 15
 
@@ -27,6 +27,11 @@ class PomodoroApp(tk.Tk):
         self.title(WINDOW_TITLE)
         self.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
         self.config(bg=BG_COLOR)
+
+        # --- Timer Variables ---
+        self.current_seconds = WORK_MIN * 60
+        self.timer_running = False
+        self.timer_thread = None
 
         # --- Load the Tomato Image ---
         image_path = os.path.join("assets","pomodoro.png")
@@ -69,7 +74,7 @@ class PomodoroApp(tk.Tk):
             font=(FONT_NAME, 14, "bold"),
             bg= BUTTON_COLOR,
             fg=FG_COLOR,
-            command=None # Fill later
+            command= self.start_timer_thread
         )
         self.start_button.pack(side=tk.LEFT, padx=10)
 
@@ -81,9 +86,62 @@ class PomodoroApp(tk.Tk):
             font = (FONT_NAME, 14, "bold"),
             bg = BUTTON_COLOR,
             fg = FG_COLOR,
-            command=None # Fill later
+            command= self.reset_timer
         )
         self.reset_button.pack(side=tk.LEFT, padx=10)
+
+    def start_timer_thread(self):
+        # Dont Start Timer if One Already On
+        if self.timer_running:
+            return
+        
+        self.timer_running = True
+
+        self.timer_thread = threading.Thread(
+            target = self.countdown_logic, #Hiring a worker to run the count_down logic
+            daemon=True
+        )
+        self.timer_thread.start()
+    
+    def countdown_logic(self):
+
+        while self.current_seconds > 0 and self.timer_running:
+            #Calculate the minutes and seconds
+            mins, secs = divmod(self.current_seconds, 60)
+
+            #Format Time String
+            time_string = f"{mins:02}:{secs:02}"
+
+            # Update The Label's Text
+            self.timer_label.config(text=time_string)
+
+            # Wait for a Second
+            time.sleep(1)
+
+            # Decrementing the time
+            self.current_seconds -= 1
+
+        self.timer_label.config(text="00:00")
+
+        # --- After the loop ---
+        if self.timer_running:
+            self.timer_running = False
+            print("Timer Finished!") # Break Logic
+
+    def reset_timer(self):
+
+        #Fire the worker thread by setting False
+        self.timer_running = False
+
+        #Reset to the Beginning
+        self.current_seconds = WORK_MIN * 60
+
+        # Manually Update the Label to show reset time
+        self.timer_label.config(text=f"{WORK_MIN:02}:00")
+    
+
+
+    
 
 
 
